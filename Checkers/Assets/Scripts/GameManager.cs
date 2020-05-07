3b1a027e281aea7b769308b9bddbd0242d91ffdc
Summer <sgbanister@gmail.com>
2020-05-06 21:50:08 -0700

    Edited Checkers

--- Checkers/Assets/Scripts/GameManager.cs
+++ Checkers/Assets/Scripts/GameManager.cs
@@ -1,15 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Specialized;
+using System.Dynamic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     public GameObject piece;
 
-    // Start is called before the first frame update
+    private GameObject[,] positions = new GameObject[8, 8];
+    private GameObject[] player = new GameObject[12];
+    private GameObject[] enemy = new GameObject[12];
+
+    private string currentPlayer = "red";
+
+    private bool gameOver = false;
+
     void Start()
     {
-        Instantiate(piece, new Vector3(0, 0, -1), Quaternion.identity);
+        player = new GameObject[] { Create("whiteChecker", 0, 7), Create("whiteChecker", 2, 7), Create("whiteChecker", 4, 7), Create("whiteChecker", 6, 7), Create("whiteChecker", 1, 6),
+                                   Create("whiteChecker", 3, 6), Create("whiteChecker", 5, 6), Create("whiteChecker", 7, 6), Create("whiteChecker", 0, 5), Create("whiteChecker", 2, 5),
+                                   Create("whiteChecker", 4, 5), Create("whiteChecker", 6, 5) };
+
+        enemy = new GameObject[] { Create("redChecker", 0, 0) };
     }
 }
