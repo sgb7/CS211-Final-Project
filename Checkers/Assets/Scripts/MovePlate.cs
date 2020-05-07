dd951fab9ae8d572acc12f56c7bb27445cd8f96b
Summer <sgbanister@gmail.com>
2020-05-07 10:24:48 -0700

    Added some code, made a new prefab

---
+++ Checkers/Assets/Scripts/MovePlate.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlate : MonoBehaviour
+{
+    public GameObject controller;
+
+    GameObject reference = null;
+
+    int matrixX;
+    int matrixY;
+
+    public bool attack = false; // Make MovePlate go from green to red when attacking?
+
+    public void Start()
+    {
+        if(attack)
+        {
+            gameObject.GetComponent<SpriteRenderer>().color = new Color(1.0f, 0.0f, 0.0f, 1.0f);
+        }
+    }
+
+
+}
