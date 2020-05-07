4c9ae938650e63299d9344a7ea1542b5c0c20b7d
Summer <sgbanister@gmail.com>
2020-05-06 21:00:06 -0700

    Edited Checkers

--- Checkers/Assets/Scripts/Checkers.cs
+++ Checkers/Assets/Scripts/Checkers.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using UnityEngine;
 
 public class Checkers : MonoBehaviour
@@ -15,10 +16,40 @@ public class Checkers : MonoBehaviour
     public Sprite redChecker, redKingChecker;
     public Sprite whiteChecker, whiteKingChecker;
 
+    public void SetCoords()
+    {
+        float x = xPos;
+        float y = yPos;
+
+        x = x * 0.66f;
+        y = y * 0.66f;
+
+        x += -2.3f;
+        y += -2.3f;
+
+        this.transform.position = new Vector3(x, y, -1.0f);
+    }
+
     public void Activate()
     {
         controller = GameObject.FindGameObjectWithTag("GameController");
 
         SetCoords();
+
+        switch(this.name)
+        {
+            case "redChecker":
+                this.GetComponent<SpriteRenderer>().sprite = redChecker;
+                break;
+            case "redKingChecker":
+                this.GetComponent<SpriteRenderer>().sprite = redKingChecker;
+                break;
+            case "whiteChecker":
+                this.GetComponent<SpriteRenderer>().sprite = whiteChecker;
+                break;
+            case "whiteKingChecker":
+                this.GetComponent<SpriteRenderer>().sprite = whiteKingChecker;
+                break;
+        }
     }
 }
