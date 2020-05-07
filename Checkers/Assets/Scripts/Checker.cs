b18a6b0170c7bf6946b9972d3c611246e66b91e7
Summer <sgbanister@gmail.com>
2020-05-06 18:27:59 -0700

    Edited Checkers

--- Checkers/Assets/Scripts/Checker.cs
+++ Checkers/Assets/Scripts/Checker.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Checker : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
--- Checkers/Assets/Scripts/GameManager.cs
+++ Checkers/Assets/Scripts/GameManager.cs
@@ -4,14 +4,10 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject piece;
+
     // Start is called before the first frame update
     void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
     {
         
     }
