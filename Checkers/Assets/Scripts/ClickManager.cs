698c5719d0dc4a7d4be5003a40c3d95cfd244323
Summer <sgbanister@gmail.com>
2020-05-07 14:04:27 -0700

    Edited Checkers

---
+++ Checkers/Assets/Scripts/ClickManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class ClickManager : MonoBehaviour
+{
+    public GameObject checker;
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            //Instantiate(checker, new Vector3(0, 0, -1), Quaternion.identity);
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+
+            RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+
+            if(hit.collider != null)
+            {
+                Instantiate(checker, new Vector3(0, 0, -1), Quaternion.identity);
+            }
+        }
+    }
+}
