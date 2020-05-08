6ba90b9ac890567c29450f8da356873a843816ae
Summer <sgbanister@gmail.com>
2020-05-08 10:09:10 -0700

    Edited Checkers

---
+++ Checkers/Assets/Scripts/DragAndDrop.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class DragAndDrop : MonoBehaviour
+{
+    float distance = 8;
+
+    void OnMouseDrag()
+    {
+        Vector3 mouseposition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance);
+        Vector3 objPosition = Camera.main.ScreenToWorldPoint(mouseposition);
+        transform.position = objPosition;
+    }
+}
